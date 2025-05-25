ff87bf6852ad6aee3fdc58819e73d269a34086f0
Tim Roeder <timothyjroeder@gmail.com>
2025-05-25 17:21:06 -0600

    add with sorted nums pattern

--- count_pairs_whose_sum_is_less_than_target_2824/solution.go
+++ count_pairs_whose_sum_is_less_than_target_2824/solution.go
@@ -1,16 +1,37 @@
 package countPairs
 
-func countPairs(nums []int, target int) int {
-	count := 0
+import "sort"
 
-	for l := 0; l < len(nums)-1; l++ {
-		r := len(nums) - 1
-		for l < r {
-			if nums[l]+nums[r] < target {
-				count++
-			}
+// initial without sorting
+// func countPairs(nums []int, target int) int {
+// 	count := 0
+
+// 	for l := 0; l < len(nums)-1; l++ {
+// 		r := len(nums) - 1
+// 		for l < r {
+// 			if nums[l]+nums[r] < target {
+// 				count++
+// 			}
+// 			r--
+// 		}
+// 	}
+// 	return count
+// }
+
+// with sorting
+func countPairs(nums []int, target int) int {
+	sort.Ints(nums)
+	count := 0
+	l, r := 0, len(nums)-1
+
+	for l < r {
+		if nums[l]+nums[r] < target {
+			count += r - l
+			l++
+		} else {
 			r--
 		}
 	}
+
 	return count
 }
