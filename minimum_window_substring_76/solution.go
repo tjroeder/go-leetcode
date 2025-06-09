a1d22ee0ec595686feb6201b4e536089ea35dcea
Tim Roeder <timothyjroeder@gmail.com>
2025-06-08 21:38:22 -0600

    redo minimum window substring again

--- minimum_window_substring_76/solution.go
+++ minimum_window_substring_76/solution.go
@@ -1,78 +1,143 @@
 package minimumwindowsubstring
 
-// my looong but working leetcode, think it is a little more readible
-// though than the one below I found on leetcode.
-func minWindow(s, t string) string {
-	// check initial conditions
-	if s == "" || t == "" || len(s) < len(t) {
+func minWindow(s1, s2 string) string {
+	// check initial cond
+	if len(s1) < len(s2) || s2 == "" {
 		return ""
 	}
 
-	// make a freqency map of the characters we are looking for
-	tMap := make(map[rune]int)
-	for _, char := range t {
-		tMap[char]++
+	// make character freq map
+	freq := make(map[byte]int)
+	for i := range s2 {
+		freq[s2[i]]++
 	}
 
-	sLen, count := len(s), len(t)
-	// minSubLen could also be math.MaxInt or int(^uint(0)>>1) which is
-	// bitwise NOT (^) unit(0) then shifted to the right one to get rid
-	// of the leading zero. Then converted to int... I think it's easier to
-	// just know that we shouldn't be greater than the length of s...
-	minSubLen := sLen + 1
-	left, right := 0, 0
-	startIdx := 0
+	// initalize variables
+	s1L, count := len(s1), len(s2)
+	minSubStringLen := s1L + 1
+	left, right, start := 0, 0, 0
 
-	// move left through s to find the left bound, while right will
-	// do the right.
-	for right < sLen {
-		// if we get a hit on the map we always decrement the character value
-		if _, ok := tMap[rune(s[right])]; ok {
-			// we only decrement the count though if we are still >0 on the character values
-			// this is because if the value is negative we don't want to double count
-			// that character and make us think that we have less characters to find
-			if tMap[rune(s[right])] > 0 {
+	// iterate right bound to the end of s1
+	for right < s1L {
+		// check if the right bound is in the freq map
+		if _, ok := freq[s1[right]]; ok {
+			// if the value being decremented is greater than zero decrement count
+			if freq[s1[right]] > 0 {
 				count--
 			}
-			tMap[rune(s[right])]--
+			// if it is in the map decrement the count
+			freq[s1[right]]--
 		}
-		// move pointer to the right
 		right++
 
-		// if we have no characters left to find enter a loop to see if we
-		// can shrink the window
+		// if the count is zero, lets try and make a smaller window by moving left
 		for count == 0 {
-			// if the left and right bounds of the window are less than the
-			// last substring length, set the startIdx to the left bound of
-			// the window, this in combination with the length will give the
-			// substring in the end rather than continuously updating a substring
-			// variable
-			if right-left < minSubLen {
-				startIdx = left
-				minSubLen = len(s[left:right])
+			// check if we have a smaller substring
+			if len(s1[left:right]) < minSubStringLen {
+				// if it is smaller we need to set the start index of the min window
+				// and set the new minSubStringLen
+				start = left
+				minSubStringLen = len(s1[left:right])
 			}
 
-			// if there is a hit on the left pointer, increase the character value before
-			// we move the pointer to the right
-			if _, ok := tMap[rune(s[left])]; ok {
-				tMap[rune(s[left])]++
-				// only increase the count if we are not negative on the character value
-				// this is because if we are still 0 or negative it means we have already
-				// met our desired amount for the substring
-				if tMap[rune(s[left])] > 0 {
+			// before moving left bound lets see what character is being kicked out of
+			// the window
+			if _, ok := freq[s1[left]]; ok {
+				// if the value being kicked out of the window is one we are looking for
+				// and we needed it to statisfy s2 substring we need to increase count
+				// so we can continue moving the right bound
+				if freq[s1[left]] == 0 {
 					count++
 				}
+				// value kicked out of the window needs to be increased
+				freq[s1[left]]++
 			}
+			// move the left bound towards the end
 			left++
 		}
 	}
 
-	// this only happens if we don't find a matching substring(t) in s
-	if minSubLen > sLen {
+	// if our minSubStringLen was never updated it will be greater than s1L so we
+	// need to return empty string because we never found a substring
+	if minSubStringLen > s1L {
 		return ""
 	}
-	return s[startIdx : startIdx+minSubLen]
+	return s1[start : start+minSubStringLen]
 }
+
+// old leetcode, think it is a little more readible
+// though than the one below I found on leetcode.
+// func minWindow(s, t string) string {
+// 	// check initial conditions
+// 	if s == "" || t == "" || len(s) < len(t) {
+// 		return ""
+// 	}
+
+// 	// make a freqency map of the characters we are looking for
+// 	tMap := make(map[rune]int)
+// 	for _, char := range t {
+// 		tMap[char]++
+// 	}
+
+// 	sLen, count := len(s), len(t)
+// 	// minSubLen could also be math.MaxInt or int(^uint(0)>>1) which is
+// 	// bitwise NOT (^) unit(0) then shifted to the right one to get rid
+// 	// of the leading zero. Then converted to int... I think it's easier to
+// 	// just know that we shouldn't be greater than the length of s...
+// 	minSubLen := sLen + 1
+// 	left, right := 0, 0
+// 	startIdx := 0
+
+// 	// move left through s to find the left bound, while right will
+// 	// do the right.
+// 	for right < sLen {
+// 		// if we get a hit on the map we always decrement the character value
+// 		if _, ok := tMap[rune(s[right])]; ok {
+// 			// we only decrement the count though if we are still >0 on the character values
+// 			// this is because if the value is negative we don't want to double count
+// 			// that character and make us think that we have less characters to find
+// 			if tMap[rune(s[right])] > 0 {
+// 				count--
+// 			}
+// 			tMap[rune(s[right])]--
+// 		}
+// 		// move pointer to the right
+// 		right++
+
+// 		// if we have no characters left to find enter a loop to see if we
+// 		// can shrink the window
+// 		for count == 0 {
+// 			// if the left and right bounds of the window are less than the
+// 			// last substring length, set the startIdx to the left bound of
+// 			// the window, this in combination with the length will give the
+// 			// substring in the end rather than continuously updating a substring
+// 			// variable
+// 			if right-left < minSubLen {
+// 				startIdx = left
+// 				minSubLen = len(s[left:right])
+// 			}
+
+// 			// if there is a hit on the left pointer, increase the character value before
+// 			// we move the pointer to the right
+// 			if _, ok := tMap[rune(s[left])]; ok {
+// 				tMap[rune(s[left])]++
+// 				// only increase the count if we are not negative on the character value
+// 				// this is because if we are still 0 or negative it means we have already
+// 				// met our desired amount for the substring
+// 				if tMap[rune(s[left])] > 0 {
+// 					count++
+// 				}
+// 			}
+// 			left++
+// 		}
+// 	}
+
+// 	// this only happens if we don't find a matching substring(t) in s
+// 	if minSubLen > sLen {
+// 		return ""
+// 	}
+// 	return s[startIdx : startIdx+minSubLen]
+// }
 
 // little more elegant version from leetcode
 // this one just uses an int slice with 128 ints for the
@@ -91,7 +156,6 @@ func minWindow(s, t string) string {
 // 	count := len(t)
 // 	start, end := 0, 0
 // 	minLen, startIndex := int(^uint(0)>>1), 0
-// 	/// UPVOTE !
 // 	for _, char := range t {
 // 		mapS[char]++
 // 	}
